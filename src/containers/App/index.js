2177b4e3c782e0d5eba2f492948b624f8b9d769d
ChonnyChu <mama.whowho@gmail.com>
2018-01-04 01:03:15 +0800

    feat(App):  use react-helmet set titleTemplate and  meta tags

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 import renderRoutes from 'react-router-config/renderRoutes';
 import { ThemeProvider } from 'styled-components';
 import Header from '../../components/Header';
@@ -12,6 +13,20 @@ class App extends React.PureComponent {
     return (
       <ThemeProvider theme={theme}>
         <div>
+          <Helmet
+            titleTemplate="%s - React Starter Boilerplate"
+            defaultTitle="React Starter Boilerplate"
+          >
+            <meta
+              name="description"
+              content="A starter kit for universal react app"
+            />
+            <meta name="og:title" content="React Starter Boilerplate" />
+            <meta
+              name="og:description"
+              content="A starter kit for universal react app"
+            />
+          </Helmet>
           <Header />
           {renderRoutes(route.routes)}
           <Footer />
