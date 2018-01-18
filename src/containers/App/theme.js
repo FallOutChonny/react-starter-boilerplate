43fbf708e0b9df8dfbd3ae8dad73fd968836eb5c
ChonnyChu <mama.whowho@gmail.com>
2018-01-18 15:04:31 +0800

    chore(Theme): add comment

--- src/containers/App/theme.js
+++ src/containers/App/theme.js
@@ -1,3 +1,7 @@
+/**
+ * Global theme variables, you can access them within styled-components
+ * via prop.theme, such as prop.theme.brandPrimary.
+ */
 export default {
   brandPrimary: '#62dafa',
   brandSecondary: '#b4d8fc',
