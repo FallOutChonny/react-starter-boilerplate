c89a6deaefcaf6463edb22819c45b8fb439e34c0
ChonnyChu <mama.whowho@gmail.com>
2018-03-26 10:05:19 +0800

    Replace superagent with fetch

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,34 +1,15 @@
-import request from 'superagent';
+export default (url, options) => {
+  const fullUrl = url; // do your staff
 
-function parseJSON(response) {
-  if (response.status === 204 || response.status === 205) {
-    return null;
-  }
-  return response.body;
-}
+  return fetch(fullUrl, options).then(response =>
+    response.json().then(json => {
+      if (!response.ok) {
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
 
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
-
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
-}
-
-function callApi(method, url, options) {
-  const { params, accept } = options;
-  return request[method](url)
-    .query(params)
-    .set('Accept', accept || 'json')
-    .then(checkStatus)
-    .then(parseJSON);
-}
-
-export default {
-  post: (url, options = {}) => callApi('POST', url, options),
-  get: (url, options = {}) => callApi('GET', url, options),
-  put: (url, options = {}) => callApi('PUT', url, options),
-  delete: (url, options = {}) => callApi('DELETE', url, options),
+      return json;
+    })
+  );
 };
