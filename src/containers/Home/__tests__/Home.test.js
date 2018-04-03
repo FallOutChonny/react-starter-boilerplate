5866db37c6aee4a1fb13b0a50bd87939486eae9f
ChonnyChu <mama.whowho@gmail.com>
2018-04-03 16:34:40 +0800

    fix(Home.test): remove "should have a h1 title" testcase

--- src/containers/Home/__tests__/Home.test.js
+++ src/containers/Home/__tests__/Home.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Home from '../Home';
 import Intro from '../Intro';
 
 describe('<Home />', () => {
   const renderComponent = shallow(<Home />);
-
-  it('should have a h1 title', () => {
-    expect(mount(<Home />).find('h1').length).toBe(1);
-  });
 
   it('should render Intro', () => {
     expect(renderComponent.find(Intro).length).toBe(1);
